c17af97bbb732553f8099ba388d206081d0b78be
Ebenezer Ejeh <ebenezeronuche@gmail.com>
2024-12-27 13:09:10 +0100

    add register, login, getUser, api endpoints.

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,40 +1,11 @@
 import dotenv from 'dotenv';
 import app from './app.js';
 import connectDB from './config/db.js';
-import User from './models/userModel.js';
 
 //load environment variables from .env
 dotenv.config()
 connectDB()
 
-const seedUsers = async()=>{
-
-    try {
-        // await User.deleteMany(); // Clears existing data
-        const users = [
-          {
-            name: "Admin User",
-            email: "admin@example.com",
-            // password: "123456",
-            // isAdmin: true,
-          },
-          {
-            name: "John Doe",
-            email: "john@example.com",
-            // password: "123456",
-            // isAdmin: false,
-          },
-        ];
-        await User.insertMany(users);
-        console.log("Users seeded successfully!");
-        process.exit();
-      } catch (error) {
-        console.error("Error seeding users:", error);
-        process.exit(1);
-      }
-    };
-
-    seedUsers();
 
 
 const PORT = process.env.PORT || 5000;
